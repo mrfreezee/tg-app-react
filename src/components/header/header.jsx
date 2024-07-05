50356f3ff9d13de02b73f37eff12165f4553e108
mrfreezee <138147783+mrfreezee@users.noreply.github.com>
2024-07-05 13:38:39 +0300

    second commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,17 @@
+const Header = () =>{
+    const tg = window.Telegram.WebApp
+
+    const onClose = () =>{
+        tg.close()
+    }
+    return(
+        <div>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    )
+}
+
+export default Header
