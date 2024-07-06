3bf685339c81148d0b13ed33765ef3f11b2537e3
mrfreezee <138147783+mrfreezee@users.noreply.github.com>
2024-07-06 14:07:08 +0300

    test commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ const Header = () =>{
     
     return(
         <div className="header">
-            <Button onClick={onClose}>Закрыть</Button>
+            <Button className='header-button' onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {user?.username}
             </span>
