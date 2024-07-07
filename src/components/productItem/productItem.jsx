8f77b9e6b891712ce1bd7415ea0dfb4f519c0bcb
mrfreezee <138147783+mrfreezee@users.noreply.github.com>
2024-07-07 15:23:00 +0300

    test commit

---
+++ src/components/productItem/productItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './productItem.css'
+import Button from '../button/button';
+    
+const ProductItem = (product, className, onAdd) => {
+
+    const onAddHandler = () =>{
+        onAdd(product)
+    }
+    return (
+        <div className={'product' + className}>
+            <div className='img'>
+                <img/>
+            </div>
+            <div className='title'>
+                {product.title}
+            </div>
+            <div className='description'>
+                {product.discritpion}
+            </div>
+            <div className='price'>
+                <span>
+                    Стоимость: <b>{product.price}</b>
+                </span>
+            </div>
+            <Button className='add-btn' onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
+            
+        </div>
+    );
+};
+
+export default ProductItem;
--- src/components/productList/productList.jsx
+++ src/components/productList/productList.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './productList.css'
+import ProductItem from '../productItem/productItem';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const products = [
+    {id:1, title: 'Майка', price: 50, description: 'Белая'},
+    {id:2, title: 'Шорты', price: 63, description: 'Черные'},
+    {id:3, title: 'Джинсы', price: 70, description: 'Темно-синие'},
+    {id:4, title: 'Кепка', price: 20, description: 'Белая'},
+    {id:5, title: 'Майка', price: 40, description: 'Желтая'},
+    {id:6, title: 'Майка', price: 45, description: 'Синяя'}
+]
     
+
+    const getTotalPrice = (items = []) =>{
+        return items.reduce((acc, item) =>{
+            return acc += item.price
+        }, 0)
+    }
 const ProductList = () => {
+    
+    const [addedItems, setAddedItems] = useState([])
+
+    const {tg} = useTelegram()
+
+    const onAdd = (product) =>{
+        const alreadyAdded = addedItems.find(item => item.id === product.id)
+        let newItems = []
+
+        if(alreadyAdded){
+            newItems = addedItems.filter(item => item.id !== product.id)
+        } else{
+            newItems = [...addedItems, product]
+        }
+        setAddedItems(newItems)
+
+        if(newItems.length === 0){
+            tg.MainButton.hide()
+        } else{
+            tg.MainButton.show()
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return (
-        <div>
-            PRLIST
+        <div className='list'>
+            {products.map(item =>(
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className='item'
+                />
+            ))}
         </div>
     );
 };
